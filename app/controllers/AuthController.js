66525505333adecef45c3b9c00c78c12348b6d5b
wassim <wassimwassim.ka1@gmail.com>
2025-10-08 15:01:00 +0100

    feat: create login function using session

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -1,4 +1,3 @@
-import mongoose from "mongoose";
 import User from "../models/User.js";
 import bcrypt, { hash } from "bcrypt";
 export default class AuthController{
@@ -29,7 +28,7 @@ export default class AuthController{
         }
 
         if(errors.length > 0){
-            res.status(400).json({error: errors});
+            return res.status(400).json({error: errors});
         }
 
         const checkEmail = await User.findOne({email: email});
@@ -53,7 +52,37 @@ export default class AuthController{
         req.session.email = user.email;
 
         const userResponse = user.toObject();
-        res.status(201).json(userResponse);
+        return res.status(201).json(userResponse);
     }   
+
+    static async login(req, res){
+        const email = req.body.email;
+        const password = req.body.password;
+        const errors = [];
+        if(!email){
+            errors.push("Email is required!");
+        }
+        if(!password){
+            errors.push("Password is required!");
+        }
+        if(errors.length >0){
+            return res.status(400).json({"errors": errors});
+        }
+        const checkExict = await User.findOne({email: email});
+        if(!checkExict){
+            return res.status(400).json({message: "Email doesn't exict!"});
+        }
+        const getPassword = checkExict.password;
+        const checkPassword = await bcrypt.compare(password, getPassword);
+        if(!checkPassword){
+            return res.status(400).json({message: "Invalid Email or Password!"});
+        }
+
+        req.session.userId = checkExict.id;
+        req.session.password = checkExict.password;
+
+        return res.status(200).json({"message": "Login Successfuly!"});
+
+    }
 
 }
--- routes/web.js
+++ routes/web.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 
 router.post("/register",AuthController.register);
+router.post("/login", AuthController.login);
 
 
 export default router;
